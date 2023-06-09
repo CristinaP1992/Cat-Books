a436dc8a1a2bc59bb0706d3f1012e7dc7a776fa8
Cristina Pavlutina <chrisciolac@gmail.com>
2023-06-09 12:52:41 +0200

    feat: added some styling

--- solo-project/src/components/BookList/BookItem/BookItem.jsx
+++ solo-project/src/components/BookList/BookItem/BookItem.jsx
@@ -7,11 +7,11 @@ export const BookItem = ({ book, updateUserBook }) => {
       <div className={cl.icons}>
         {book.readingModes.image ? (
           <div>
-            <img src={book.imageLinks.smallThumbnail} alt="" />
+            <img src={book.imageLinks.thumbnail} alt="" />
           </div>
         ) : (
           <div>
-            <img src="/cover.jpg" alt="alt-cover" />
+            <img className={cl.cover} src="/cover.jpg" alt="alt-cover" />
           </div>
         )}
         {/* {userBook.favorite ? (
@@ -29,53 +29,49 @@ export const BookItem = ({ book, updateUserBook }) => {
             onClick={() => updateUserBook(book.id, 'favorite')}
           />
         )} */}
-        {userBook.status === 'toread' ? (
-          <div
-            className={cl.wanttoread}
-            onClick={() => updateUserBook(book.id, 'toread')}
-          >
-            Want to read
-          </div>
-        ) : (
-          <div
-            className={cl.read}
-            onClick={() => updateUserBook(book.id, 'toread')}
-          >
-            Want to read
-          </div>
-        )}
-        {userBook.status === 'reading' ? (
-          <div
-            className={cl.reading}
-            onClick={() => updateUserBook(book.id, 'reading')}
-          >
-            Reading now
-          </div>
-        ) : (
-          <div
-            className={cl.notreading}
-            onClick={() => updateUserBook(book.id, 'reading')}
-          >
-            Reading now
-          </div>
-        )}
-        {userBook.status === 'read' ? (
-          <div
-            className={cl.completed}
-            onClick={() => updateUserBook(book.id, 'read')}
-          >
-            Read
-          </div>
-        ) : (
-          <div
-            className={cl.complete}
-            onClick={() => updateUserBook(book.id, 'read')}
-          >
-            Read
-          </div>
-        )}
+        <div className={cl.icons2}>
+          {userBook.status === 'toread' ? (
+            <div onClick={() => updateUserBook(book.id, 'toread')}>
+              <img src="./toread2.png" alt="" />
+            </div>
+          ) : (
+            <div onClick={() => updateUserBook(book.id, 'toread')}>
+              <img src="./toread.png" alt="" />
+            </div>
+          )}
+          {userBook.status === 'reading' ? (
+            <div
+              className={cl.reading}
+              onClick={() => updateUserBook(book.id, 'reading')}
+            >
+              <img src="./reading-now2.png" alt="" />
+            </div>
+          ) : (
+            <div
+              className={cl.notreading}
+              onClick={() => updateUserBook(book.id, 'reading')}
+            >
+              <img src="./reading2.png" alt="" />
+            </div>
+          )}
+          {userBook.status === 'read' ? (
+            <div
+              className={cl.completed}
+              onClick={() => updateUserBook(book.id, 'read')}
+            >
+              <img src="./read2.png" alt="" />
+            </div>
+          ) : (
+            <div
+              className={cl.complete}
+              onClick={() => updateUserBook(book.id, 'read')}
+            >
+              <img src="./read.png" alt="" />
+            </div>
+          )}
+        </div>
       </div>
-      <div>
+      <div className={cl.description}>
         <h3>{book.title}</h3>
         <h3 style={{ fontWeight: 'normal' }}>{book.authors}</h3>
         <p className={cl.max_lines}>{book.description}</p>
--- solo-project/src/components/Discover/Discover.jsx
+++ solo-project/src/components/Discover/Discover.jsx
@@ -1,10 +1,15 @@
 import { BookList } from '../BookList/BookList';
+import cl from './Discover.module.css';
 
 export const Discover = ({ books, updateUserBook }) => {
   return (
     <>
-      <h2>Books for you</h2>
-      <BookList books={books} updateUserBook={updateUserBook} />
+      <div className={cl.books}>
+        <h2>Books for you</h2>
+      </div>
+      <div className={cl.booklist}>
+        <BookList books={books} updateUserBook={updateUserBook} />
+      </div>
     </>
   );
 };
