9a5812ee20081e68c1b63606999b872aeda977f8
Cristina Pavlutina <chrisciolac@gmail.com>
2023-06-12 09:03:16 +0200

    feat: added category

--- solo-project/src/components/Discover/Discover.jsx
+++ solo-project/src/components/Discover/Discover.jsx
@@ -1,11 +1,14 @@
 import { useEffect } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
 import { BookList } from '../BookList/BookList';
 import cl from './Discover.module.css';
 import { useBooks } from '../../useBooks';
 
 export const Discover = () => {
   const { books, updateUserBook, fetchBooksByCategory } = useBooks();
-  const category = 'cats';
+  const { category } = useParams();
+
+  const getActive = ({ isActive }) => (isActive ? cl.active : cl.item);
 
   useEffect(() => {
     fetchBooksByCategory(category);
@@ -15,6 +18,28 @@ export const Discover = () => {
     <>
       <div className={cl.books}>
         <h2>Books for you</h2>
+      </div>
+      <div className={cl.category}>
+        <div className={cl.item}>
+          <NavLink to="/discover/cats" className={getActive}>
+            Cats
+          </NavLink>
+        </div>
+        <div className={cl.item}>
+          <NavLink to="/discover/fiction" className={getActive}>
+            Fiction
+          </NavLink>
+        </div>
+        <div className={cl.item}>
+          <NavLink to="/discover/detective" className={getActive}>
+            Detective
+          </NavLink>
+        </div>
+        <div className={cl.item}>
+          <NavLink to="/discover/fantasy" className={getActive}>
+            Fantasy
+          </NavLink>
+        </div>
       </div>
       <div className={cl.booklist}>
         <BookList books={books} updateUserBook={updateUserBook} />
